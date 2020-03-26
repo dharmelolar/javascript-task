e614fba0386f3f6ccd07ec0024d047ba2d58e98d
Damilola Ezekiel <hardeydoyeen1@gmail.com>
2020-03-26 16:46:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let myName="damilola ezekiel";
+let myCourses =['front-end','design','coding'];
+
+console.log(myName);
+console.log(myCourses);
+
+for (let myCourses=1; myCourses<=200; myCourses+=
+    2){
+    console.log(myCourses);
+}
